1b3086dda099392c391ede69d162e5e37eb27a2f
hassamshaikh <hassamshaikh94@gmail.com>
2025-02-17 17:15:34 +0500

    learned animationcurves

---
+++ swiftuithinking/swiftuithinking/AnimationTiming.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationTiming.swift
+//  swiftuithinking
+//
+//  Created by Hassam Shaikh on 17/02/2025.
+//
+
+import SwiftUI
+
+struct AnimationTiming: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationTiming()
+}
