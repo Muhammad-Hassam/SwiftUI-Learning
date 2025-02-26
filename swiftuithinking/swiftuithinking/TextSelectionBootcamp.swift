1ebc189a5c54d98a22987c5f923f111a7c7587a9
hassamshaikh <hassamshaikh94@gmail.com>
2025-02-26 14:33:12 +0500

    learned textSelection

---
+++ swiftuithinking/swiftuithinking/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  swiftuithinking
+//
+//  Created by Hassam Shaikh on 26/02/2025.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
