412a81bf03a04bff49433c389b069177948e23cd
hassamshaikh <hassamshaikh94@gmail.com>
2025-02-27 17:11:42 +0500

    learned menu

---
+++ swiftuithinking/swiftuithinking/MenuBootcamp.swift
@@ -0,0 +1,31 @@
+//
+//  MenuBootcamp.swift
+//  swiftuithinking
+//
+//  Created by Hassam Shaikh on 27/02/2025.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Click Me !"){
+            Button("One"){
+                
+            }
+            Button("Two"){
+                
+            }
+            Button("Three"){
+                
+            }
+            Button("Four"){
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuBootcamp()
+}
